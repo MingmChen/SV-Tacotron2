2dd9cfee58171bc3600a7b539874cd2ea7af6960
Zhengxi Liu <39415743+xcmyz@users.noreply.github.com>
2019-03-14 00:12:10 +0800

    Add files via upload

---
+++ V1/hparams.py
@@ -0,0 +1,33 @@
+# Audio
+num_mels = 40
+num_freq = 257
+sample_rate = 16000
+frame_length_ms = 25
+frame_shift_ms = 10
+tisv_frame = 180
+preemphasis = 0.97
+min_level_db = -100
+ref_level_db = 20
+griffin_lim_iters = 60
+power = 1.5
+
+# Model
+n_mels_channel = 40
+hidden_dim = 768
+num_layer = 3
+speaker_dim = 256
+re_num = 1e-6
+
+# Train
+dataset_path = "./dataset"
+dataset_test_path = "./dataset_test"
+origin_data = "D:/wav48"
+total_utterance = 200
+N = 2  # batch_size
+M = 10
+learning_rate = 0.01
+epochs = 500
+checkpoint_path = "./model_new"
+save_step = 500
+log_step = 5
+clear_Time = 20
---
+++ V1/preprocess.py
@@ -0,0 +1,65 @@
+import os
+import threading
+from tqdm import tqdm
+import numpy as np
+from concurrent.futures import ProcessPoolExecutor
+from multiprocessing import cpu_count
+from functools import partial
+
+import audio
+import hparams as hp
+
+
+def preprocess(speaker_id, speaker_file_name, class_num=200, data_path=hp.origin_data):
+    out_dataset = hp.dataset_path
+    if not os.path.exists(out_dataset):
+        os.mkdir(out_dataset)
+
+    file_path = os.path.join(data_path, speaker_file_name)
+    wav_file_list = os.listdir(file_path)[0:class_num]
+
+    for utterance_id, wav_file in enumerate(wav_file_list):
+        wav_file_path = os.path.join(file_path, wav_file)
+        wav = audio.load_wav(wav_file_path)
+        mel_spec = audio.melspectrogram(wav)
+
+        save_file_name = str(speaker_id) + "_" + str(utterance_id) + ".npy"
+        np.save(os.path.join(out_dataset, save_file_name), mel_spec)
+
+
+def preprocess_test(speaker_id, speaker_file_name, start_num=200, data_path=hp.origin_data):
+    out_dataset = hp.dataset_test_path
+    if not os.path.exists(out_dataset):
+        os.mkdir(out_dataset)
+
+    file_path = os.path.join(data_path, speaker_file_name)
+    wav_file_list = os.listdir(file_path)[start_num:]
+
+    for utterance_id, wav_file in enumerate(wav_file_list):
+        wav_file_path = os.path.join(file_path, wav_file)
+        wav = audio.load_wav(wav_file_path)
+        mel_spec = audio.melspectrogram(wav)
+
+        save_file_name = str(speaker_id) + "_" + str(utterance_id) + ".npy"
+        np.save(os.path.join(out_dataset, save_file_name), mel_spec)
+
+
+if __name__ == "__main__":
+
+    # # preprocess(0, "p225")
+    # list_speaker = os.listdir(hp.origin_data)
+    # # thrs = [threading.Thread(target=preprocess, args=[speaker_id, file_name])
+    # #         for speaker_id, file_name in enumerate(list_speaker)]
+    # # [thr.start() for thr in thrs]
+    # # [thr.join() for thr in thrs]
+
+    # executor = ProcessPoolExecutor(max_workers=cpu_count())
+    # futures = [executor.submit(partial(preprocess, speaker_id, file_name))
+    #            for speaker_id, file_name in enumerate(list_speaker)]
+    # [future.result() for future in futures]
+
+    list_speaker = os.listdir(hp.origin_data)
+    executor = ProcessPoolExecutor(max_workers=cpu_count())
+    futures = [executor.submit(partial(preprocess_test, speaker_id, file_name))
+               for speaker_id, file_name in enumerate(list_speaker)]
+    [future.result() for future in futures]
